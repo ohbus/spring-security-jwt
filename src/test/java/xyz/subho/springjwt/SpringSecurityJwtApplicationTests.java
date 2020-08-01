0b81a761cb80d0b9e41ad525ce0ea1be7d092d1e
Subhrodip Mohanta <contact@subho.xyz>
2020-08-01 14:29:42 +0530

    Initial Commit

---
+++ src/test/java/xyz/subho/springjwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.subho.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
