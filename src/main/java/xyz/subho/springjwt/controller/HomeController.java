4d4f7cfa2b7aaf1917bd35f502826a444740b07a
Subhrodip Mohanta <contact@subho.xyz>
2020-08-02 14:38:09 +0530

    added Controllers

---
+++ src/main/java/xyz/subho/springjwt/controller/HomeController.java
@@ -0,0 +1,18 @@
+package xyz.subho.springjwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home()	{
+		return "Home!";
+	}
+	
+	@RequestMapping("/hello")
+	public String hello()	{
+		return "Hello World";
+	}
+}
